2bf0ea799bd468b663e22e9cdb6c63a9065fea3d
sheetal <xebia@xe-tu-sheetal.xebiaindia.local>
2015-09-12 17:49:29 +0530

    second test commit

--- src/main/java/collections/Test.java
+++ src/main/java/collections/Test.java
@@ -2,8 +2,12 @@ package collections;
 
 public class Test {
 
+	public void test(){
+		
+	}
+	
 	public static void main(String [] args){
-		System.out.println("Hi");
+		System.out.println("Hi Git");
 	}
 	
 }
