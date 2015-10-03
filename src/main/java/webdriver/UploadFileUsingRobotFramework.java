ab693378a9701e9ea520a8ec9a2b673e9f226d36
sheetal <sheetal0123@gmail.com>
2015-10-03 14:36:31 +0530

    robot

---
+++ src/main/java/webdriver/UploadFileUsingRobotFramework.java
@@ -0,0 +1,67 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/*
+ * Robot class is a java class which take control of Mouse and Keyboard
+ */
+public class UploadFileUsingRobotFramework {
+	WebDriver driver;
+	DriversRepo repo;
+
+	@BeforeTest
+	public void setup() throws Exception {
+		repo = new DriversRepo();
+		driver = repo.getMozillaDriver();
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void uploadTest() throws InterruptedException {
+		driver.get("http://only-testing-blog.blogspot.in/2014/01/textbox.html");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.name("img")).click();
+		robotFunc("F:\\softwares\\autoit\\testupload.txt");
+		Thread.sleep(4000);
+		System.out.println("Title: " + driver.getTitle()); 
+	}
+
+	/*
+	 * This method will copy file path to system's clip-board.
+	 */
+	public static void setClipboardData(String string) {
+		// StringSelection is a class that can be used for copy and paste operations
+		StringSelection stringSelection = new StringSelection(string);
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+	}
+
+	public static void robotFunc(String fileLocation) {
+		try {
+			setClipboardData(fileLocation);
+			Robot robot = new Robot();
+
+			// paste path
+			robot.keyPress(KeyEvent.VK_CONTROL);
+			robot.keyPress(KeyEvent.VK_V);
+			robot.keyRelease(KeyEvent.VK_V);
+			robot.keyRelease(KeyEvent.VK_CONTROL);
+
+			// press enter
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
