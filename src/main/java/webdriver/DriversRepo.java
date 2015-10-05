c89f2ce9c27b19d81bf971c54716f7fa14f12fae
sheetal <xebia@xe-tu-sheetal.xebiaindia.local>
2015-10-05 17:52:39 +0530

    grid changes 5 oct

--- src/main/java/webdriver/DriversRepo.java
+++ src/main/java/webdriver/DriversRepo.java
@@ -1,29 +1,33 @@
 package webdriver;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
 
 public class DriversRepo {
 
 	static WebDriver driver;
-	
-	public static WebDriver getMozillaDriver(){
+
+	public static WebDriver getMozillaDriver() {
 		return new FirefoxDriver();
 	}
-	
-	public static WebDriver getChromeDriver(){
-		System.setProperty("webdriver.chrome.driver", "F:\\softwares\\jars\\chromedriver.exe");
-		return new ChromeDriver();   //chrome driver version= 2.16
+
+	public static WebDriver getChromeDriver() {
+		if (System.getProperty("os.name").contains("Linux")) {
+			System.setProperty("webdriver.chrome.driver",
+					"/home/xebia/Downloads/jars/chromedriver");
+			return new ChromeDriver();
+		}
+		System.setProperty("webdriver.chrome.driver",
+				"F:\\softwares\\jars\\chromedriver.exe");
+		return new ChromeDriver();
 	}
-	
-	public WebDriver getIEDriver(){
-		System.setProperty("webdriver.ie.driver",  "F:\\softwares\\jars\\IEDriverServer.exe");
+
+	public WebDriver getIEDriver() {
+		System.setProperty("webdriver.ie.driver",
+				"F:\\softwares\\jars\\IEDriverServer.exe");
 		return new InternetExplorerDriver();
 	}
-	
-	
-	
+
 }
