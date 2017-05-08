6368cfa980fd1053d6bbb58ff7de47123c41709b
sheetal singh <sheetalsingh@xebia.com>
2017-05-08 09:02:10 +0530

    thread local

---
+++ src/main/java/interviewpractice/todo.java
@@ -0,0 +1,20 @@
+package interviewpractice;
+
+public class todo {
+/*
+	good practice site: 
+	http://javaconceptoftheday.com/java-interview-programs-on-strings/
+	geeksforgeek
+
+
+	Ques:
+	1. find whether a string is anagram or not
+	2. reverse a string using recursion
+	3. find the max count no [2,3,2,4,1,2,3,2,2,2,2,2]  o/p = 2
+
+
+	Books:
+	Data Structures and Algorithms Made Easy in Java by Narasimha Karumanchi  | for Algo Complexity etc, vipul jain 
+	The Java Language Specification by James Gosling, Bill Joy etc | for Java , vipul jain
+*/
+}
