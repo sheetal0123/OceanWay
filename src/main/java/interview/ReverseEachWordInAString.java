9787b7197b3072828d7c8eed7f1650a4303019d4
Singh <Sheetal.Singh@mastercard.com>
2018-03-13 10:43:29 +0530

    rev string

--- src/main/java/interview/ReverseEachWordInAString.java
+++ src/main/java/interview/ReverseEachWordInAString.java
@@ -1,7 +1,11 @@
 package interview;
 
+
 public class ReverseEachWordInAString {
 
+	/*
+	 * Expected output: avaJ tpecnoC fO ehT yaD 
+	 */
 	static void reverseEachWordOfString(String inputString) {
 
 		String[] arrWords = inputString.split(" ");
@@ -22,7 +26,43 @@ public class ReverseEachWordInAString {
 		System.out.println(reverseString);
 	}
 
+	
+	
+
+	/*
+	 * Expected output: Day The Of Concept Java
+	 */
+	static void reverseStringWordWise(String inputString) {
+		String [] arr = inputString.split(" ");
+		
+		for(int i= arr.length - 1; i>= 0; i--){
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println(" ");
+	}
+	
+	
+	
+	/*
+	 * Expected output: yaD ehT fO tpecnoC avaJ 
+	 */
+	static void reverseEvertything(String inputString){
+		String [] arr = inputString.split(" ");
+		
+		for(int i= arr.length - 1; i>=0; i--){
+			String word = arr[i];
+			for(int j= word.length()- 1; j>=0;j--){
+					System.out.print(word.charAt(j));
+			}
+			System.out.print(" ");
+		}
+		
+	}
+
+	
 	public static void main(String[] args) {
 		reverseEachWordOfString("Java Concept Of The Day");
+		reverseStringWordWise("Java Concept Of The Day");
+		reverseEvertything("Java Concept Of The Day");
 	}
 }
