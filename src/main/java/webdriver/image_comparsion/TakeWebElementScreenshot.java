fb6832e5361b39b9bdbe00746b700ce0c66f4b57
sheetal <sheetal0123@gmail.com>
2018-12-01 15:13:11 +0530

    screenshot

--- src/main/java/webdriver/image_comparsion/TakeWebElementScreenshot.java
+++ src/main/java/webdriver/image_comparsion/TakeWebElementScreenshot.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
+import webdriver.DriverRepo;
 
 public class TakeWebElementScreenshot {
 
@@ -20,8 +20,12 @@ public class TakeWebElementScreenshot {
 WebDriver driver = null;
 	
 	WebDriver getDriver(){
-		if(driver == null)
-			driver = new FirefoxDriver();
+		if(driver == null){
+			System.out.println("Driver is null, hence initializing ...");
+			driver = DriverRepo.FIREFOX.getDriver();
+			//driver = DriverRepo.CHROME.getDriver();
+		}
+		driver.manage().window().maximize();
 		return driver;
 	}
 	
@@ -31,12 +35,43 @@ WebDriver driver = null;
 	
 	
 	/**
+	 * This method will take complete page screenshot
+	 * 
+	 * In both FF/CR only view port screenshot is captured
+	 */
+	public void takeCompletePageScreenShot(String screenshot_path) {
+		System.out.println("Wait for page to get load");
+		try {
+			Thread.sleep(2000);
+
+			File screenshotFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+			BufferedImage completePageImage = ImageIO.read(screenshotFile);
+
+			ImageIO.write(completePageImage, "png", screenshotFile);
+
+			// copy screenshot to given file path
+			File screenshotLocation = new File(screenshot_path);
+			FileUtils.copyFile(screenshotFile, screenshotLocation);
+
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	/**
 	 * This method will take specific element screenshot and return same
 	 * 
 	 * For future comparison output of this method will be sent to compare method 
+	 * 
 	 */
-	public BufferedImage takeWebElementScreenshot(WebElement webelement, String screenshot_path) {
+	public BufferedImage takeWebElementScreenshot(WebElement webelement, String screenshot_path)   {
 		try {
+			System.out.println("Wait for page to get load");
+			Thread.sleep(5000);
 			File screenshotFile = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
 			BufferedImage completePageImage = ImageIO.read(screenshotFile);
 
@@ -53,20 +88,42 @@ WebDriver driver = null;
 			FileUtils.copyFile(screenshotFile, screenshotLocation);
 
 			return weblementScreenshot;
-		} catch (IOException e) {
+		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
 	
 	
+
+	public void closeDriver(){
+		getDriver().close();
+	}
+	
 	public static void main(String[] args) {
 		TakeWebElementScreenshot obj = new TakeWebElementScreenshot();
 		obj.openPage("https://en.wikipedia.org/wiki/Buddhism");
 		
-		WebElement webelement = obj.getDriver().findElement(By.xpath("//div[@id='toc']"));
-		String result_path = ".\\src\\main\\java\\webdriver\\image_comparsion\\element_screenshot.png";
-		obj.takeWebElementScreenshot(webelement, result_path);
+		String fullpath = ".\\src\\main\\java\\webdriver\\image_comparsion\\full.png";
+		obj.takeCompletePageScreenShot(fullpath);
+		
+		
+		
+		//div[@id='mw-content-text']   complete page
+		//div[@id='p-logo']/a   logo , visible in view port
+		//div[@id='mw-panel']/div[2]/div    left upper pod, visible in view port
+		//div[@id='toc']  middle pod : partially visible in view port
+		//div[@id='mw-content-text']/div/table  , not visible in view port
+		//div[@id='mw-content-text']/div/table[5]  , not visible in view port
+		
+		
+//		WebElement webelement = obj.getDriver().findElement(By.xpath("//div[@id='mw-content-text']/div/table[6]"));
+//		String result_path = ".\\src\\main\\java\\webdriver\\image_comparsion\\end2end.png";
+//		obj.takeWebElementScreenshot(webelement, result_path);
+	
+		
+		obj.closeDriver();
+		System.out.println("Done");
 	}
 
 }
